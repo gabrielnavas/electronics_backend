4d509d653d4c31dd187abc2f79e209b858e64e94
Navas <gabrielnavas@email.com>
2021-04-03 17:39:00 -0300

    getall use case tests

--- __tests__/domain/usecase/clients/get-all.spec.ts
+++ __tests__/domain/usecase/clients/get-all.spec.ts
@@ -1,6 +1,5 @@
 import { Client } from '../../../../src/domain/models/client'
 import { getAll } from '../../../../src/domain/usecase/clients/get-all'
-import { client } from '../../../../src/infra/database/postgres_business/connection'
 
 describe('getAll UseCase', () => {
   test('should call clienteGetAllRepository', async () => {
@@ -8,5 +7,32 @@ describe('getAll UseCase', () => {
     const sut = await getAll(clientGetAllRepositorySpyFn)
     await sut()
     expect(clientGetAllRepositorySpyFn).toBeCalledTimes(1)
+  })
+
+  test('should return many clients', async () => {
+    const clients = [{
+      id: 'any_id1',
+      name: 'any_name1',
+      email: 'any_email1@email.com',
+      password: 'any_password1'
+    }, {
+      id: 'any_id2',
+      name: 'any_name2',
+      email: 'any_email2@email.com',
+      password: 'any_password2'
+    }]
+    const clientRepository = async (): Promise<Client[]> => {
+      return [...clients]
+    }
+    const sut = await getAll(clientRepository)
+    const clientsResult = await sut()
+    expect(clientsResult).toEqual(clients)
+  })
+
+  test('should throws if clientGetAllRepository throws', async () => {
+    const clientGetAllRepositorySpyFn = jest.fn().mockRejectedValueOnce(new Error())
+    const sut = await getAll(clientGetAllRepositorySpyFn)
+    const promise = sut()
+    expect(promise).rejects.toThrow()
   })
 })
