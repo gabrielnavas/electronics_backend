0a5647cc0b2168f97c3b1fdbccdcacc04332968b
Navas <gabrielnavas@email.com>
2021-04-04 16:23:18 -0300

    fix port name

--- src/main/configs/env/index.ts
+++ src/main/configs/env/index.ts
@@ -9,7 +9,7 @@ export default {
   database: {
     postgres_business: {
       host: helpers.isDevOrTest() ? 'localhost' : process.env.POSTGRES_HOST,
-      port: helpers.isDevOrTest() ? 5432 : Number(process.env.PORT),
+      port: helpers.isDevOrTest() ? 5432 : Number(process.env.POSTGRES_PORT),
       user: helpers.isDevOrTest() ? 'postgres' : process.env.POSTGRES_USER,
       password: helpers.isDevOrTest() ? '123' : process.env.PASSWORD,
       database: helpers.isDevOrTest() ? 'postgres' : process.env.DATABASE
