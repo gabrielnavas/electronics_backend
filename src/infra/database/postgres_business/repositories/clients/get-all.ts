48bc12e660d18cda9f542424821b82b3f9e8c8f3
Navas <gabrielnavas@email.com>
2021-04-07 23:41:59 -0300

    fix line less promise.resolve

--- src/infra/database/postgres_business/repositories/clients/get-all.ts
+++ src/infra/database/postgres_business/repositories/clients/get-all.ts
@@ -3,12 +3,13 @@ import { Client } from '../../../../../domain/models/client'
 
 import { client as clientConnection } from '../../connection'
 
-export const getAllClientsPostgres: ClientGetAllRepository = async (): Promise<Client[]> => {
-  const sql = `
-    SELECT id, name, email, password
-    FROM eletronics_navas.client;
-  `
-  const clients = await clientConnection.query<Client[]>(sql)
+export const getAllClientsPostgres: ClientGetAllRepository =
+  async (): Promise<Client[]> => {
+    const sql = `
+      SELECT id, name, email, password
+      FROM eletronics_navas.client;
+    `
+    const clients = await clientConnection.query<Client[]>(sql)
 
-  return Promise.resolve(clients)
-}
+    return clients
+  }
