5fcf1e741e667b82fa67daf882ff491fbf1c781e
Navas <gabrielnavas@email.com>
2021-04-07 23:21:51 -0300

    check model client in controller

--- src/presentation/controllers/client/create-client-controller.ts
+++ src/presentation/controllers/client/create-client-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
+import { Client, validate } from '../../../domain/models/client'
 import { clientCreateOne } from '../../../domain/usecase/clients/create-one'
 import { insertOneClientPostgres } from '../../../infra/database/postgres_business/repositories/clients/insert-one'
+import { isEmail } from '../../../infra/validation/validate'
 
 const createClientController = async (req: Request, res: Response) => {
   try {
@@ -18,12 +20,21 @@ const createClientController = async (req: Request, res: Response) => {
         .json(new Error('password is different from password confirmation'))
     }
 
-    const client = await clientCreateOne(insertOneClientPostgres)({
+    const clientOrError: Client | Error = validate(isEmail)({
       name: req.body.name,
       password: req.body.password,
       email: req.body.email
     })
-    const { password, ...clientLessPassword } = client
+
+    if (clientOrError instanceof Error) {
+      return res.status(400)
+        .json(clientOrError)
+    }
+
+    const clientCreated = await clientCreateOne(
+      insertOneClientPostgres
+    )(clientOrError)
+    const { password, ...clientLessPassword } = clientCreated
     res.status(201).json(clientLessPassword)
   } catch (error) {
     res.status(500).json(new Error('server error.'))
